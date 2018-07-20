31e780725631f082b01049f09ff611a16624d152
Tam Le <40102136+tamnle@users.noreply.github.com>
2018-07-19 23:03:34 -0500

    Create index.php

---
+++ pages/index.php
@@ -0,0 +1,53 @@
+<?php include 'header.php';?>
+
+    <div class="header">
+      <img src="pictures/face.png" alt="profile picture">
+    <h1>
+      <strong><strong><small>Tam N. Le</small></strong></strong>
+      <small><small><small><small><br/>
+        Double Majors in Accounting & Computer Science
+
+        <br/><u>Current Position:</u>
+        <br/>&emsp; &emsp; Bookkeeper at Graves & Oberlies CPAs
+
+        <br/><u>Interested in:</u>
+        <ul>
+          <li>Big Data</li>
+          <li>Data Science</li>
+          <li>Data Analyst</li>
+          <li>Data Architect</li>
+        </ul>
+      </small></small></small></small>
+    </h1>
+    </div>
+
+
+<!-- End the top part of the page-->
+
+    <div class="main">
+
+      <h3><strong>WHY I AM INTERESTED IN BIG DATA?</strong></h3>
+
+      <p>
+        Within the rise of technology, Big Data is becoming one of the biggest concern for most businesses around the world.
+        According to this, businesses will need data experts to manage their data system.
+        However, most of business experts do not have a strong computing background, while computer
+        scientists are mostly not familiar with business field. Therefore, the gap is created.
+        And, my goal is fulfilling this gap.
+        <br/><br/>
+        To accomplish this goal, I'm going to study both B.S. in Computer Science and B.S.B.A. in Accounting.
+        I believe that even this preparation step may take a lot of time, but it will be trustworthy.
+        <br/><br/>
+        <em>If we could do our best, there should be no reason to make things just as good.</em>
+      </p>
+
+    </div>
+<!--End the greeting part-->
+
+<?php include 'emailform.php';?>
+
+  </body>
+<!--End of the entire body of the page-->
+
+
+ <?php include 'footer.php';?>
